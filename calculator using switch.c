0688955a4a3f3cb430d48d99f6b294f7164bebe0
DHANUSH V <dhanushv9060@gmail.com>
2025-12-03 21:00:20 +0530

    Refactor calculator code for clarity and structure

--- calculator using switch.c
+++ calculator using switch.c
@@ -1,16 +1,16 @@
 //CLACULATOR TO PERFORM ADDITION,MULTIPLICATION,DIVISION,SUBTACTION
-#include<stdio.h>
-#include<math.h>
-int main()
+#include<stdio.h>                                  //HEADER FILE
+#include<math.h>                                   //HEADER FILE
+int main()                                         //MAIN FUCTION
 {
-  float variable1,variable2;
-  int choice;
+  float variable1,variable2;                       //DECLARATION OF VARIABLE IN FLAOTE DATA TYPE
+  int choice;                                      //DECLARATION OF VARIABLE IN INTEGER DATA TYPE
   printf("ENTER TWO VALUES TO PERFORM OPERATION\n");
-  scanf("%f%f",&variable1,&variable2);
-  printf("SELECT THE OPERATION TO PERFORM:\n");
+  scanf("%f%f",&variable1,&variable2);               //TAKING INPUT FROM USER
+  printf("SELECT THE OPERATION TO PERFORM:\n");      //TAKING OPERATION CHOICE FROM USER
   printf("1.ADDITION\n2.SUBTRACTION\n3.MULTIPLICATION\n4.DIVISION\n");
   scanf("%d",&choice);
-  switch(choice)
+  switch(choice)                                            //USING SWITCH FUNCTION TO PERFORM DIFFERENT TASKS
 {
 case 1://ADDITION
     printf("%.2f + %.2f = %.2f",variable1,variable2,variable1+variable2);
@@ -28,6 +28,7 @@ default:
     printf("YOUR CHOICE IS WRONG!!");
     break;
 }
+return 0;
 }
 
 
